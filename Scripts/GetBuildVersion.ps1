368acd8e4029788b8fd1151e846fd678f2ee4e5c
Alexandru Macocian <almacoci@microsoft.com>
2021-05-05 17:32:28 +0200

    Set release configuration in cd pipeline.

--- Scripts/GetBuildVersion.ps1
+++ Scripts/GetBuildVersion.ps1
@@ -1,6 +1,7 @@
 Function GetBuildVersion{
     Write-Host "Retrieving version"
     $filepath = Get-ChildItem -Path .\Daybreak -Filter *.version
+    Write-Host $filepath
     $version = $filepath.BaseName
     Write-Host "Version: $version"
     return $version | Out-String
