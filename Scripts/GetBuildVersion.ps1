c95b2795fb4ed278261d3e2cf98eba2dd91a8b57
Alexandru Macocian <almacoci@microsoft.com>
2021-05-05 19:12:36 +0200

    a

--- Scripts/GetBuildVersion.ps1
+++ Scripts/GetBuildVersion.ps1
@@ -1,7 +1,5 @@
-Function GetBuildVersion{
-    Write-Host "Retrieving version"
-    $filepath = Get-ChildItem -Path .\Daybreak -Filter *.version
-    $version = $filepath.BaseName
-    Write-Host "Version: $version"
-    return $version
-}
+Write-Host "Retrieving version"
+$filepath = Get-ChildItem -Path .\Daybreak -Filter *.version
+$version = $filepath.BaseName
+Write-Host "Version: $version"
+return $version
