6776803528ce1e8344beded51b87df7b0a58d036
Alexandru Macocian <amacocian@yahoo.com>
2022-08-13 00:13:16 +0200

    Ignore 'v' in front of version value

--- Scripts/CompareVersions.ps1
+++ Scripts/CompareVersions.ps1
@@ -5,6 +5,14 @@ Param(
     [string]$lastVersion
 )
 
+if ($currentVersion.StartsWith("v")){
+    $currentVersion = $currentVersion.Substring(1)
+}
+
+if ($lastVersion.StartsWith("v")){
+    $lastVersion = $lastVersion.Substring(1)
+}
+
 $isNewer = $currentVersion.CompareTo($lastVersion) -eq 1
 if ($isNewer -eq $false){
     throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
