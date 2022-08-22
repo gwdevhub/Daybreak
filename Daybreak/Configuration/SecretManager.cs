c9904e15e6f0c0168f438dee5c91deb76cc6a46a
Alexandru Macocian <amacocian@yahoo.com>
2022-08-22 14:40:58 +0200

    Setup support for secrets

---
+++ Daybreak/Configuration/SecretManager.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Extensions;
+using System.Linq;
+using System.Reflection;
+
+namespace Daybreak.Configuration;
+
+public static class SecretManager
+{
+    private static JObject SecretsHolder;
+
+    public static string GetSecret(SecretKeys secretKey)
+    {
+        if (SecretsHolder is null)
+        {
+            LoadSecrets();
+        }
+
+        return SecretsHolder.Value<string>(secretKey.Key);
+    }
+
+    private static void LoadSecrets()
+    {
+        var serializedSecrets = Assembly.GetExecutingAssembly().GetManifestResourceStream("Daybreak.secrets.json").ReadAllBytes().GetString();
+        serializedSecrets = TrimUnwantedCharacters(serializedSecrets);
+        SecretsHolder = JObject.Parse(serializedSecrets);
+    }
+
+    private static string TrimUnwantedCharacters(string s)
+    {
+        return new string(s.Where(c =>
+            char.IsWhiteSpace(c) ||
+            char.IsLetterOrDigit(c) ||
+            char.IsSymbol(c) ||
+            char.IsSeparator(c) ||
+            char.IsPunctuation(c))
+            .ToArray());
+    }
+}
