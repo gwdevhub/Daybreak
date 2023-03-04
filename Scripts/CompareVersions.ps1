0f3538277254174ad8e727e92c689c52abbee8dc
Alexandru Macocian <amacocian@yahoo.com>
2023-03-04 17:01:13 +0100

    Bugfix version comparison script

--- Scripts/CompareVersions.ps1
+++ Scripts/CompareVersions.ps1
@@ -13,10 +13,36 @@ if ($lastVersion.StartsWith("v")){
     $lastVersion = $lastVersion.Substring(1)
 }
 
-$isNewer = $currentVersion.CompareTo($lastVersion) -eq 1
-if ($isNewer -eq $false){
-    throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
+$currentVersionTokens = $currentVersion.Split('.')
+$lastVersionTokens = $lastVersion.Split('.')
+if ($currentVersionTokens.Length -eq $lastVersionTokens.Length)
+{
+    for($i = 0 ; $i -lt $currentVersionTokens.Length; $i++)
+    {
+        $currentVersionToken = [int]$currentVersionTokens[$i]
+        $lastVersionToken = [int]$lastVersionTokens[$i]
+        if ($currentVersionToken -lt $lastVersionToken){
+            throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
+        }
+    }
+
+    if ($currentVersionToken -le $lastVersionToken){
+        throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
+    }
+
+    Write-Host "Version has been incremented"
 }
-else{
+elseif ($currentVersionTokens.Length -gt $lastVersionTokens.Length){
+    if ($currentVersionTokens[$lastVersionTokens.Length - 1] -lt $lastVersionTokens[$lastVersionTokens.Length - 1]){
+        throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
+    }
+
+    Write-Host "Version has been incremented"
+}
+elseif ($currentVersionTokens.Length -lt $lastVersionTokens.Length){
+    if ($currentVersionTokens[$currentVersionTokens.Length - 1] -le $lastVersionTokens[$currentVersionTokens.Length - 1]){
+        throw "Version is not incremented. Current version " + $currentVersion + ". Last version " + $lastVersion
+    }
+
     Write-Host "Version has been incremented"
 }
