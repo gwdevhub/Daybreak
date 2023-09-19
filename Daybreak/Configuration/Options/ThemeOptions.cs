ff94e9025988cb55475208349aaa095bae196d2a
Macocian Alexandru Victor <amacocian@yahoo.com>
2023-09-19 10:20:52 +0200

    Toggle background effects (#375)

--- Daybreak/Configuration/Options/ThemeOptions.cs
+++ Daybreak/Configuration/Options/ThemeOptions.cs
@@ -11,4 +11,10 @@ public sealed class ThemeOptions
 
     [OptionName(Name = "Color Scheme", Description = "The client will apply the selected color scheme")]
     public ColorScheme Theme { get; set; } = ColorScheme.Steel;
+
+    [OptionName(Name = "Paintify Background", Description = "If true, will apply a shader to make the background image look like a painting")]
+    public bool BackgroundPaintify { get; set; } = true;
+
+    [OptionName(Name = "Blur Background", Description = "If true, the background image will be blurred")]
+    public bool BackgroundBlur { get; set; } = false;
 }
