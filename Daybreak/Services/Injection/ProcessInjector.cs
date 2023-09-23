3335dae9d000359f221adced62f27075d0b417df
Alexandru Macocian <amacocian@yahoo.com>
2023-09-23 03:26:20 +0200

    Use Reloaded.Injector to avoid cross WoW64 boundary issues Closes #399

--- Daybreak/Services/Injection/ProcessInjector.cs
+++ Daybreak/Services/Injection/ProcessInjector.cs
@@ -1,10 +1,8 @@
-﻿using Daybreak.Utils;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using Microsoft.Extensions.Logging;
+using Reloaded.Injector;
 using System.Core.Extensions;
 using System.Diagnostics;
 using System.Extensions;
-using System.Text;
 
 namespace Daybreak.Services.Injection;
 internal sealed class ProcessInjector : IProcessInjector
@@ -20,52 +18,10 @@ internal sealed class ProcessInjector : IProcessInjector
     public bool Inject(Process process, string pathToDll)
     {
         var scopedLogger = this.logger.CreateScopedLogger(nameof(this.Inject), pathToDll);
-        scopedLogger.LogInformation("Getting process handle");
-        var processHandle = NativeMethods.OpenProcess(
-            NativeMethods.ProcessAccessFlags.CreateThread |
-            NativeMethods.ProcessAccessFlags.QueryInformation |
-            NativeMethods.ProcessAccessFlags.VMOperation |
-            NativeMethods.ProcessAccessFlags.VMRead |
-            NativeMethods.ProcessAccessFlags.VMWrite,
-            false,
-            (uint)process.Id);
-
-        if (processHandle == IntPtr.Zero)
-        {
-            return false;
-        }
-
-        scopedLogger.LogInformation("Allocating address for dll path");
-        var dwSize = pathToDll.Length + 1;
-        var allocatedAddress = NativeMethods.VirtualAllocEx(
-            processHandle,
-            IntPtr.Zero,
-            (uint)dwSize,
-            NativeMethods.MemoryAllocationType.MEM_COMMIT | NativeMethods.MemoryAllocationType.MEM_RESERVE,
-            NativeMethods.MemoryProtection.PAGE_READWRITE);
-        if (allocatedAddress == IntPtr.Zero)
-        {
-            return false;
-        }
-
-        scopedLogger.LogInformation("Writing dll path to process memory");
-        var stringBytes = Encoding.Default.GetBytes(pathToDll);
-        if (!NativeMethods.WriteProcessMemory(processHandle, allocatedAddress, stringBytes, (uint)dwSize, out var bytesWritten))
-        {
-            return false;
-        }
-
-        scopedLogger.LogInformation("Getting kernel32.dll handle");
-        var kernel32Handle = NativeMethods.GetModuleHandle("kernel32.dll");
-        scopedLogger.LogInformation("Getting address of LoadLibraryA");
-        var loadLibraryA = NativeMethods.GetProcAddress(kernel32Handle, "LoadLibraryA");
-        scopedLogger.LogInformation("Creating remote thread");
-        var threadHandle = NativeMethods.CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryA, allocatedAddress, 0, out _);
-        scopedLogger.LogInformation("Cleaning up allocated memory");
-        NativeMethods.VirtualFreeEx(processHandle, IntPtr.Zero, dwSize, NativeMethods.MemoryAllocationType.MEM_RESET);
-        scopedLogger.LogInformation("Closing process handle");
-        NativeMethods.CloseHandle(processHandle);
-
-        return true;
+        scopedLogger.LogInformation("Injecting");
+        var injector = new Injector(process);
+        var result = injector.Inject(pathToDll);
+        scopedLogger.LogInformation($"Injection finished with result {result}");
+        return result != 0;
     }
 }
