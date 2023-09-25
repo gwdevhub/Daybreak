793d60f7b549106b49d4ff807cc4776577cd1ca8
Macocian Alexandru Victor <amacocian@yahoo.com>
2023-09-25 16:22:52 +0200

    Add tooltips for buttons (#409)
    Closes #379

--- Daybreak/Models/Options/OptionSection.cs
+++ Daybreak/Models/Options/OptionSection.cs
@@ -4,5 +4,6 @@ namespace Daybreak.Models.Options;
 public sealed class OptionSection
 {
     public string? Name { get; init; }
+    public string? Tooltip { get; init; }
     public Type? Type { get; init; }
 }
