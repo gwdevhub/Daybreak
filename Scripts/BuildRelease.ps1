d11eb3996281b1287883c954c4de50e6f62eed0f
Alex Macocian <amacocian@yahoo.com>
2023-11-21 22:02:35 +0100

    Improve update procedure

--- Scripts/BuildRelease.ps1
+++ Scripts/BuildRelease.ps1
@@ -1,19 +1,30 @@
-Param(
-    [Parameter(Mandatory=$true)]
-    [string]$version
-) #end param
+function Get-FileMetadata {
+    param (
+        [string]$Path
+    )
+
+    $fileInfo = Get-Item $Path
+
+    # Temporarily change current location to the folder path
+    $currentLocation = Get-Location
+    Set-Location -Path .\Publish
+    $relativePath = Resolve-Path -Path $Path -Relative
+    Set-Location -Path $currentLocation
+    return @{
+        Name         = $fileInfo.Name
+        Size         = $fileInfo.Length
+        RelativePath = $relativePath.trim(".\\")
+    }
+}
 
 Write-Output "Deleting pdb file"
 Remove-item .\Publish\Daybreak.pdb
 Remove-item .\Publish\Daybreak.Installer.pdb
 Remove-item .\Publish\Daybreak.7ZipExtractor.pdb
 Move-Item -Path .\Publish\Daybreak.Installer.exe -Destination .\Publish\Daybreak.Installer.Temp.exe
-$zipPath = "Publish\daybreakv$version.zip"
-Write-Output "Compressing binaries to $zipPath"
-Compress-Archive .\Publish\* $zipPath -Force
-Write-Output "Cleaning up files"
-foreach ($file in Get-ChildItem -Path .\Publish -Exclude *.zip -Recurse -ErrorAction SilentlyContinue)
-{
-    Write-Output "Removing $file"
-    Remove-item $file -Recurse -Force -ErrorAction SilentlyContinue
-}
+
+$files = Get-ChildItem -Path .\Publish -Recurse -File
+$metadata = $files | ForEach-Object { Get-FileMetadata $_.FullName }
+$json = $metadata | ConvertTo-Json
+$json | Out-File -FilePath ".\Publish\Metadata.json"
+Write-Host "Metadata written to .\Publish\Metadata.json"
