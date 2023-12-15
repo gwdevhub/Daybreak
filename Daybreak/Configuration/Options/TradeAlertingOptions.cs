8ffbb918eac085af8a8361f3a6b54048a1f62486
Macocian Alexandru Victor <amacocian@yahoo.com>
2023-12-15 14:08:51 +0100

    Add frequency checking option for trader quote alerts (#512)
    Hide sell trade quotes since they don't populate
    Closes #509
    Closes #508

--- Daybreak/Configuration/Options/TradeAlertingOptions.cs
+++ Daybreak/Configuration/Options/TradeAlertingOptions.cs
@@ -5,10 +5,19 @@ using System.Collections.Generic;
 
 namespace Daybreak.Configuration.Options;
 
-[OptionsIgnore]
+[OptionsName(Name = "Trade Alerts")]
 internal sealed class TradeAlertingOptions
 {
+    [OptionIgnore]
     public TimeSpan MaxLookbackPeriod { get; set; } = TimeSpan.FromDays(31);
+
+    [OptionIgnore]
     public DateTime LastCheckTime { get; set; } = DateTime.MinValue;
+
+    [OptionIgnore]
     public List<ITradeAlert> Alerts { get; set; } = [];
+
+    [OptionName(Name = "Quote Alerts Interval", Description = "The amount of seconds in between checking for alerts based on trade quotes")]
+    [OptionRange<double>(MinValue = 30d, MaxValue = 1440d)]
+    public double QuoteAlertsInterval { get; set; } = 30;
 }
