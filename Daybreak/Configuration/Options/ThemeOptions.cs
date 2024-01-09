2d4ca138141a1bab7de1109fcb7c279c5c56c788
Alex Macocian <amacocian@yahoo.com>
2024-01-09 13:54:33 +0200

    Disable wintersday mode

--- Daybreak/Configuration/Options/ThemeOptions.cs
+++ Daybreak/Configuration/Options/ThemeOptions.cs
@@ -21,6 +21,7 @@ public sealed class ThemeOptions
     [OptionName(Name = "Blur Background", Description = "If true, the background image will be blurred")]
     public bool BackgroundBlur { get; set; } = false;
 
+    [OptionIgnore]
     [OptionName(Name = "Wintersday Mode", Description = "If true, Daybreak will apply a Wintersday theme")]
-    public bool WintersdayMode { get; set; } = false;
+    public bool WintersdayMode => false;
 }
