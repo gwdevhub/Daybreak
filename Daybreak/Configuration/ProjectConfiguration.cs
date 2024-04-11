170f4049bdd7bc70d3b489e87ca08830b8bbd0c3
Macocian Alexandru Victor <amacocian@yahoo.com>
2024-04-11 17:43:33 +0200

    Event Calendar (#625)
    * Event calendar view

--- Daybreak/Configuration/ProjectConfiguration.cs
+++ Daybreak/Configuration/ProjectConfiguration.cs
@@ -262,6 +262,7 @@ public class ProjectConfiguration : PluginConfigurationBase
         services.AddScoped<IProcessInjector, ProcessInjector>();
         services.AddScoped<ILaunchConfigurationService, LaunchConfigurationService>();
         services.AddScoped<IBrowserHistoryManager, BrowserHistoryManager>();
+        services.AddScoped<IEventService, EventService>();
     }
 
     public override void RegisterViews(IViewProducer viewProducer)
@@ -331,6 +332,7 @@ public class ProjectConfiguration : PluginConfigurationBase
         viewProducer.RegisterView<DirectSongSwitchView>();
         viewProducer.RegisterView<SettingsSynchronizationView>();
         viewProducer.RegisterView<TeamBuildTemplateView>();
+        viewProducer.RegisterView<EventCalendarView>();
     }
 
     public override void RegisterStartupActions(IStartupActionProducer startupActionProducer)
@@ -445,6 +447,7 @@ public class ProjectConfiguration : PluginConfigurationBase
         notificationHandlerProducer.RegisterNotificationHandler<FixSymbolicLinkNotificationHandler>();
         notificationHandlerProducer.RegisterNotificationHandler<UpdateNotificationHandler>();
         notificationHandlerProducer.RegisterNotificationHandler<ReShadeConfigChangedHandler>();
+        notificationHandlerProducer.RegisterNotificationHandler<NavigateToCalendarViewHandler>();
     }
 
     public override void RegisterMods(IModsManager modsManager)
---
+++ Daybreak/Services/Events/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using Daybreak.Models.Guildwars;
+using System;
+using System.Collections.Generic;
+
+namespace Daybreak.Services.Events;
+public interface IEventService
+{
+    TimeOnly GetLocalizedEventStartTime();
+
+    ICollection<Event> GetActiveEvents(DateTime dateTime);
+
+    ICollection<Event> GetCurrentActiveEvents();
+
+    DateTime GetEventStartTime(Event e);
+
+    Event GetUpcomingEvent();
+}
