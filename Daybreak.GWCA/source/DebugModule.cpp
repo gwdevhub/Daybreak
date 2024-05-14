79da1801bc75af7acc47cd94fd2c21e1c7094b48
Macocian Alexandru Victor <amacocian@yahoo.com>
2024-05-15 01:24:08 +0200

    GWCA Debug Module (#733)

---
+++ Daybreak.GWCA/source/DebugModule.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "DebugModule.h"
+#include "payloads/DebugPayload.h"
+#include <GWCA/GWCA.h>
+#include <GWCA/Context/GameContext.h>
+
+namespace Daybreak::Modules {
+    std::string DebugModule::ApiUri()
+    {
+        return "/debug";
+    }
+
+    std::optional<uint32_t> DebugModule::GetContext(const httplib::Request& req, httplib::Response& res)
+    {
+        return 0;
+    }
+
+    std::optional<DebugPayload> DebugModule::GetPayload(const uint32_t)
+    {
+        char buffer[20];
+        DebugPayload debugPayload;
+        const auto gameContext = GW::GetGameContext();
+        std::sprintf(buffer, "%p", static_cast<void*>(gameContext));
+        debugPayload.GameContextAddress = std::string(buffer);
+        std::sprintf(buffer, "%p", static_cast<void*>(gameContext->agent));
+        debugPayload.AgentContextAddress = std::string(buffer);
+        std::sprintf(buffer, "%p", static_cast<void*>(gameContext->character));
+        debugPayload.CharContextAddress = std::string(buffer);
+        std::sprintf(buffer, "%p", static_cast<void*>(gameContext->world));
+        debugPayload.WorldContextAddress = std::string(buffer);
+        std::sprintf(buffer, "%p", static_cast<void*>(gameContext->map));
+        debugPayload.MapContextAddress = std::string(buffer);
+
+        return debugPayload;
+    }
+}
