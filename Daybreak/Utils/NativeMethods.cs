4e1c0c4f5054e698435d217d7f5fd25def29bdeb
Macocian Alexandru Victor <amacocian@yahoo.com>
2024-06-06 13:35:10 +0200

    Save dump on crash (Closes #749) (#751)

--- Daybreak/Utils/NativeMethods.cs
+++ Daybreak/Utils/NativeMethods.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using static Daybreak.Utils.NativeMethods;
 
 namespace Daybreak.Utils;
 
@@ -16,6 +17,33 @@ internal static class NativeMethods
 
     public delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
 
+    [Flags]
+    public enum MinidumpType : uint
+    {
+        MiniDumpNormal = 0x00000000,
+        MiniDumpWithDataSegs = 0x00000001,
+        MiniDumpWithFullMemory = 0x00000002,
+        MiniDumpWithHandleData = 0x00000004,
+        MiniDumpFilterMemory = 0x00000008,
+        MiniDumpScanMemory = 0x00000010,
+        MiniDumpWithUnloadedModules = 0x00000020,
+        MiniDumpWithIndirectlyReferencedMemory = 0x00000040,
+        MiniDumpFilterModulePaths = 0x00000080,
+        MiniDumpWithProcessThreadData = 0x00000100,
+        MiniDumpWithPrivateReadWriteMemory = 0x00000200,
+        MiniDumpWithoutOptionalData = 0x00000400,
+        MiniDumpWithFullMemoryInfo = 0x00000800,
+        MiniDumpWithThreadInfo = 0x00001000,
+        MiniDumpWithCodeSegs = 0x00002000,
+        MiniDumpWithoutAuxiliaryState = 0x00004000,
+        MiniDumpWithFullAuxiliaryState = 0x00008000,
+        MiniDumpWithPrivateWriteCopyMemory = 0x00010000,
+        MiniDumpIgnoreInaccessibleMemory = 0x00020000,
+        MiniDumpWithTokenInformation = 0x00040000,
+        MiniDumpWithModuleHeaders = 0x00080000,
+        MiniDumpFilterTriage = 0x00100000,
+        MiniDumpValidTypeFlags = 0x001fffff,
+    }
     [StructLayout(LayoutKind.Sequential)]
     internal struct ProcessEntry32
     {
@@ -332,6 +360,8 @@ internal static class NativeMethods
 
     public const int WM_SYSCOMMAND = 0x112;
 
+    [DllImport("Dbghelp.dll", SetLastError = true)]
+    public static extern bool MiniDumpWriteDump(IntPtr hProcess, int processId, SafeHandle hFile, MinidumpType dumpType, IntPtr expParam, IntPtr userStreamParam, IntPtr callbackParam);
     [DllImport("kernel32.dll", SetLastError = true)]
     public static extern IntPtr CreateToolhelp32Snapshot(uint dwFlags, uint th32ProcessID);
     [DllImport("kernel32.dll")]
