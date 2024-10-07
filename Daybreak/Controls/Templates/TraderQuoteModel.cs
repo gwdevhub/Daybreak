a3580e4d83d7dba6be0ebdd2a80bcd9a3b3db7ca
Alex Macocian <amacocian@yahoo.com>
2024-10-07 11:34:31 +0200

    Use sell quotes. Add sell prices and buy prices to trade quote view

---
+++ Daybreak/Controls/Templates/TraderQuoteModel.cs
@@ -0,0 +1,11 @@
+﻿using Daybreak.Models.Guildwars;
+using System;
+
+namespace Daybreak.Controls.Templates;
+public sealed class TraderQuoteModel
+{
+    public ItemBase? Item { get; set; }
+    public int BuyPrice { get; set; }
+    public int SellPrice { get; set; }
+    public DateTime TimeStamp { get; set; }
+}
