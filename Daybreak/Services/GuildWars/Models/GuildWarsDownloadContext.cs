bba0f4dff889d33117beb5e91f9f175b42865e27
Alex Macocian <amacocian@yahoo.com>
2025-04-27 15:48:50 +0200

    Rename remaining files

--- Daybreak/Services/GuildWars/Models/GuildWarsDownloadContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Daybreak.Models.Progress;
-using System.Threading;
-
-namespace Daybreak.Services.GuildWars.Models;
-internal sealed class GuildWarsDownloadContext
-{
-    public GuildwarsInstallationStatus? GuildwarsInstallationStatus { get; init; }
-    public CancellationTokenSource? CancellationTokenSource { get; init; }
-}
--- Daybreak/Services/GuildWars/Models/GuildWarsUpdateResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Daybreak.Services.GuildWars.Models;
-public sealed class GuildWarsUpdateResponse
-{
-    public bool Result { get; init; }
-    public string? ExecutablePath { get; init; }
-}
