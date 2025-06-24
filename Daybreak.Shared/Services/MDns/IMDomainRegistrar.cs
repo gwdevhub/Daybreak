5a92b916dd948bf5970417a5391a0c226c52bc57
Macocian Alexandru Victor <amacocian@yahoo.com>
2025-06-24 13:38:02 +0200

    Improve Focus View performance and hooking availability (#1009) (Closes #1007)

--- Daybreak.Shared/Services/MDns/IMDomainRegistrar.cs
+++ Daybreak.Shared/Services/MDns/IMDomainRegistrar.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 namespace Daybreak.Shared.Services.MDns;
 public interface IMDomainRegistrar
 {
-    public IReadOnlyList<Uri>? Resolve(string service);
+    IReadOnlyList<Uri>? Resolve(string service);
 
-    public IReadOnlyList<Uri>? QueryByServiceName(Func<string, bool> query);
+    IReadOnlyList<Uri>? QueryByServiceName(Func<string, bool> query);
+
+    void QueryAllServices();
 }
