66e2c5eb9514b1ab023506f45e4782f2f917bf34
Macocian Alexandru Victor <amacocian@yahoo.com>
2025-12-09 13:17:53 +0100

    Fix gMod version parser (Closes #1105) (#1109)

--- Daybreak.Shared/Services/DXVK/IDXVKService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Daybreak.Shared.Models.Async;
-using Daybreak.Shared.Services.Mods;
-
-namespace Daybreak.Shared.Services.DXVK;
-
-public interface IDXVKService : IModService
-{
-    Task<bool> SetupDXVK(IProgress<ProgressUpdate> progress, CancellationToken cancellationToken);
-}
