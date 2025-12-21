c93a8cbab2fc23e59aaefec30f7bcd824ebf4380
Alex Macocian <amacocian@yahoo.com>
2025-12-21 12:00:15 +0100

    Fix notification time conversion (Closes #1163 #1168 #1165)

--- Daybreak/Models/NotificationWrapper.cs
+++ Daybreak/Models/NotificationWrapper.cs
@@ -4,5 +4,5 @@ namespace Daybreak.Models;
 public sealed class NotificationWrapper
 {
     public required Notification Notification { get; init; }
-    public DateTimeOffset ExpirationTime { get; set; }
+    public DateTime ExpirationTime { get; set; }
 }
