be1b4c4e33486f83c095d8949df37c2cbcd0acbc
Alex Macocian <amacocian@yahoo.com>
2026-01-09 17:42:35 +0100

    Fix styles

--- Daybreak/Launch/LaunchState.cs
+++ Daybreak/Launch/LaunchState.cs
@@ -4,11 +4,15 @@ namespace Daybreak.Launch;
 
 public sealed class LaunchState
 {
-    public readonly static LaunchState Instance = new();
+    public static readonly ProgressUpdate Initializing = new(0, "Initializing");
+    public static readonly ProgressUpdate LoadingOptions = new(0.1, "Loading options");
 
-    public ProgressUpdate Progress;
+    public static event EventHandler<ProgressUpdate>? ProgressChanged;
 
-    private LaunchState()
+    public static ProgressUpdate Progress { get; private set; } = Initializing;
+
+    public static void UpdateProgress(ProgressUpdate progress)
     {
+        Progress = progress;
     }
 }
