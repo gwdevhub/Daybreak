07533b43db8c6531cdcceac08bc2513143a0e581
Alexandru Macocian <amacocian@microsoft.com>
2026-01-12 13:29:04 +0100

    Improve changelog when missing Metadata.json (Closes #1278)

---
+++ Daybreak.Shared/Models/Github/GithubRelease.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Daybreak.Shared.Models.Github;
+
+public sealed class GithubRelease
+{
+    [JsonPropertyName("id")]
+    public required int Id { get; init; }
+    [JsonPropertyName("tag_name")]
+    public required string TagName { get; init; }
+    [JsonPropertyName("body")]
+    public required string Body { get; init; }
+    [JsonPropertyName("assets")]
+    public required IReadOnlyList<GithubReleaseAsset> Assets { get; init; }
+}
