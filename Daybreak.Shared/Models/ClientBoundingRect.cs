ef7effb38f7fdfc27beeac006d97af901f94021f
Macocian Alexandru Victor <amacocian@yahoo.com>
2026-01-16 14:47:30 +0100

    Build component improvements (Closes #1292) (#1296)
    * Improve build component UX
    
    * Make builds show pop-up

---
+++ Daybreak.Shared/Models/ClientBoundingRect.cs
@@ -0,0 +1,9 @@
+﻿namespace Daybreak.Shared.Models;
+
+public sealed class ClientBoundingRect
+{
+    public double Top { get; init; }
+    public double Left { get; init; }
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
