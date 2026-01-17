f5483352f09aec3ee1079a5447f9f81b037abfa9
Macocian Alexandru Victor <amacocian@yahoo.com>
2026-01-17 23:38:31 +0100

    Add more modifiers (#1309)

--- Daybreak.Shared/Models/Guildwars/ConditionType.cs
+++ Daybreak.Shared/Models/Guildwars/ConditionType.cs
@@ -1,8 +1,31 @@
-﻿namespace Daybreak.Shared.Models.Guildwars;
+﻿using System.Text.Json.Serialization;
 
+namespace Daybreak.Shared.Models.Guildwars;
+
+[JsonConverter(typeof(JsonStringEnumConverter<ConditionType>))]
 public enum ConditionType
 {
-    Bleed,
+    Bleeding,
     Blind,
-    Cripple = 3
+    Burning,
+    Crippled,
+    DeepWound,
+    Disease,
+    Poison,
+    Dazed,
+    Weakness
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter<SkillIdToCondition>))]
+public enum SkillIdToCondition
+{
+    Bleeding = 0x1DE,
+    Blind = 0x1DF,
+    Burning = 0x1E0,
+    Crippled = 0x1E1,
+    DeepWound = 0x1E2,
+    Disease = 0x1E3,
+    Poison = 0x1E4,
+    Dazed = 0x1E5,
+    Weakness = 0x1E6
 }
