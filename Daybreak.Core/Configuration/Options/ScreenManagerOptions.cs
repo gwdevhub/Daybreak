5b6950979f1c0795b1741ea318afd7e7815acf8b
Alexandru Macocian <amacocian@microsoft.com>
2026-02-08 15:43:49 +0100

    Make application DPI aware (Closes #1369)

--- Daybreak.Core/Configuration/Options/ScreenManagerOptions.cs
+++ Daybreak.Core/Configuration/Options/ScreenManagerOptions.cs
@@ -11,4 +11,6 @@ public sealed class ScreenManagerOptions
     public double Y { get; set; }
     public double Width { get; set; }
     public double Height { get; set; }
+    public int DpiX { get; set; } = 96;
+    public int DpiY { get; set; } = 96;
 }
